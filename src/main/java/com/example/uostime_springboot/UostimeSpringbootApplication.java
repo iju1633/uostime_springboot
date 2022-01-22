bc181ba66b05f946fa4fd2b1b40ca0f8d229a415
iju1633 <iju1633@gmail.com>
2022-01-23 00:18:23 +0900

    boilerplate upload

---
+++ src/main/java/com/example/uostime_springboot/UostimeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.uostime_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UostimeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UostimeSpringbootApplication.class, args);
+	}
+
+}
